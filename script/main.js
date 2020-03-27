a42dfc520ce74cd2d04a311a9e36349625cb8941
Francesco <francesco.ferruccio88@gmail.com>
2020-03-27 14:12:03 +0100

    aggiunta funzionalità al bottone annulla

--- script/main.js
+++ script/main.js
@@ -1,8 +1,17 @@
+// DESCRIZIONE:
+// Creo una versione dell’EX del biglietto del treno ricca con un bell’output dinamico;
+// Sulla base di quello visto insieme in aula, ma non per forza identico;
+// L’importante è usare eventi sui 2 bottoni principali, e sviluppare le logiche che sottendono;
+// “Genera” mi calcolerà il prezzo del biglietto a partire dai dati inseriti negli input, e mi draà l’output in pagina, nel modo migliore possibile;
+// “Annulla” mi fa tornare allo stato iniziale con output pulito (o nascosto) e campi puliti (non vale fare refresh del browser :male-farmer:);
+
+
 // creo delle variabili a cui assegnare gli elementi in pagina
 var nome = document.getElementById('nome');
 var km = document.getElementById('km');
 var eta = document.getElementById('eta');
 var btnGenera = document.getElementById('btnGenera');
+var btnAnnulla = document.getElementById('btnAnnulla');
 
 // creo un modo per salvare il contenuto degli input al click sul bottone "Genera"
 btnGenera.addEventListener("click",
@@ -19,7 +28,6 @@ btnGenera.addEventListener("click",
     var prezzo = 0.21; //prezzo al km
     var percSconto = 0;
     var sconto = 0;
-    var msgSconto = "";
 
     // calcolo il prezzo del biglietto e lo memorizzo in una variabile
     var biglietto = contKm * prezzo;
@@ -37,5 +45,13 @@ btnGenera.addEventListener("click",
       biglietto = biglietto - sconto;
     }
     console.log(biglietto);
+  }
+)
+
+btnAnnulla.addEventListener("click",
+  function () {
+    nome.value = "";
+    km.value = "";
+    eta.value = "";
   }
 )
